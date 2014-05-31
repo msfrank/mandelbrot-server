2f83462f4da9949859fa82d3419d91846a0cc930
Michael Frank <msfrank@syntaxjockey.com>
2014-05-31 14:02:53 -0700

    bump dev version to 0.0.5

--- project/MandelbrotServerBuild.scala
+++ project/MandelbrotServerBuild.scala
@@ -4,7 +4,7 @@ import com.github.retronym.SbtOneJar
 
 object MandelbrotServerBuild extends Build {
 
-  val mandelbrotVersion = "0.0.4"
+  val mandelbrotVersion = "0.0.5"
 
   val scalaLangVersion = "2.10.4"
   val akkaVersion = "2.3.3"
