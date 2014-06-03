da4c58b4b6648eda460d3ae45c7098d858311d85
Michael Frank <msfrank@syntaxjockey.com>
2014-06-03 13:35:39 -0700

    add escalation policy (although we don't use yet)

---
+++ src/main/scala/io/mandelbrot/core/notification/EscalationPolicy.scala
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2014 Michael Frank <msfrank@syntaxjockey.com>
+ *
+ * This file is part of Mandelbrot.
+ *
+ * Mandelbrot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mandelbrot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mandelbrot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.mandelbrot.core.notification
+
+/**
+ *
+ */
+case class EscalationPolicy(behavior: RollupBehavior)
+
+sealed trait RollupBehavior
+case object AlertOnAllChildren extends RollupBehavior
+case object AlertOnAnyChild extends RollupBehavior
